b081d030c536b42fc47c8519010961e1369ff5b1
barisdongel <barisdng26@gmail.com>
2023-12-06 17:14:28 +0300

    Update student

--- src/student/controller.js
+++ src/student/controller.js
@@ -10,6 +10,7 @@ const getStudents = (req, res) => {
 
 const getStudentById = (req, res) => {
     const id = parseInt(req.params.id);
+
     pool.query(queries.getStudentById, [id], (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows);
@@ -38,17 +39,44 @@ const removeStudent = (req, res) => {
     const id = parseInt(req.params.id);
 
     //if student doesn't exist
-    pool.query(queries.removeStudent, [id], (error, results) => {
+    pool.query(queries.getStudentById, [id], (error, results) => {
         const noStudentFound = !results.rows.length;
         if (noStudentFound) {
             res.send("Student doesn't exists");
         }
     });
+
+    //Delete student
+    pool.query(queries.removeStudent, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send("Student Removed Success")
+        console.log(results.status)
+    });
+};
+
+const updateStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name, email } = req.body;
+
+    //if student doesn't exist
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound) {
+            res.send("Student doesn't exists");
+        }
+
+        pool.query(queries.updateStudent, [name, email, id], (error,results) => {
+            if (error) throw error;
+            res.status(200).send("Update success")
+            console.log("Updated version is :" + results.rows)
+        })
+    })
 };
 
 module.exports = {
     getStudents,
     getStudentById,
     addStudent,
-    removeStudent
+    removeStudent,
+    updateStudent
 };
